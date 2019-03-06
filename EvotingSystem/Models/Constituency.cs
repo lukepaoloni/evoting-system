ecd10a46dd87909c170a87fd8f9e0210a1d202c6
Luke Paoloni <lukepaoloni.me@gmail.com>
2019-03-06 19:20:58 +0000

    ...

---
+++ EvotingSystem/Models/Constituency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvotingSystem.Models
+{
+    public class Constituency
+    {
+        public String Name { get; set; }
+        public Candidate ElectedCandidate { get; set; }
+
+        public Constituency() { }
+
+        public void electCandidate(Candidate candidate)
+        {
+            this.ElectedCandidate = candidate;
+        }
+    }
+}
