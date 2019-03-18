08fc636b4c04c7c3a8d33d71a5e53f148fce3855
WardJake3 <44470284+WardJake3@users.noreply.github.com>
2019-03-18 16:55:27 +0000

    Router and Started Login Page

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+
 import './App.css';
+const err = () =>{
+  return(
+      <div>error</div>
+  )
+}
+// const User = (params) =>{
+//   return(
+//       <div>{"Welcome"} :{params.userId} </div>
+//   )
+// }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-          <HomePage />
-        </header>
-      </div>
+      <BrowserRouter>
+                   <div>
+                   {/* <Navigation/> */}
+                   <Switch>
+                    <Route path="/" exact render={()=>(
+                        (sessionStorage.getItem('user')) ? (<HomePage/>):  (<Redirect to='/login' />)
+                    )}/>
+                    <Route path="/login" component={LoginPage}/>
+                    {/* <Route path="/vote"  render={()=>(
+                        (sessionStorage.getItem('user')) ? (<DownloadView/>):  (<Redirect to='/login' />)
+                    )}/>
+                    <Route path="/list"  render={()=>(
+                        (sessionStorage.getItem('user')) ? (<Upload/>):  (<Redirect to='/login' />)
+                    )}/>
+                    <Route path="/admin/:userId"  render={({match})=>(
+                        (sessionStorage.getItem('user')) ? (<User userId={match.params.userId}/>):  (<Redirect to='/login' />)
+                    )}/> */}
+                    <Route component={err} />
+                   </Switch>
+                   </div>
+                </BrowserRouter>
     );
   }
 }
---
+++ web/src/Pages/LoginPage.jsx
@@ -0,0 +1,125 @@
+import React, {Component} from 'react';
+import '../styles/form.css';
+import {Redirect} from 'react-router-dom'
+const formValid = ({ formErrors, ...rest }) => {
+    let valid = true;
+    // validate form errors being empty
+    Object.values(formErrors).forEach(val => {
+      val.length > 0 && (valid = false);
+    });
+  
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+        
+      val === null && (valid = false);
+    });
+   
+    return valid;
+  };
+  
+export default class LoginView extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            username: null,
+            password: null,
+            loginSucc: false,
+            formErrors: {
+              username: "",
+              password: "",
+            }
+          };
+          this.handleSubmit = this.handleSubmit.bind(this)
+          this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(e){
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "username":
+        formErrors.username =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "password":
+        formErrors.password =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ formErrors, [name]: value });
+    }
+
+    componentWillMount(){
+      
+      
+    }
+    async handleSubmit(e){
+        e.preventDefault()
+        if (formValid(this.state)) {
+          
+            let Loginuserdetails = {
+                username: this.state.username,
+                password: this.state.password
+            }
+            console.log(Loginuserdetails);
+          } else {
+            alert("FORM INVALID")
+            console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+          }
+
+    }
+    
+    render(){
+        if(this.state.loginSucc)
+            return <Redirect to={'/'}/>
+        const { formErrors } = this.state;
+        return(
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <h1>Login</h1>
+                    <div >
+                        <div className="evoting">
+                            <label htmlFor="username">{"username"}</label>
+                            <input
+                                placeholder={"username"}
+                                type="text"
+                                name="username"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {formErrors.username.length > 0 && (
+                                <span className="errorMessage">{this.state.formErrors.username}</span>
+                            )}
+                        </div>
+                        <div className="evoting">
+                            <label htmlFor="password">{"password"}</label>
+                            <input
+                                placeholder={"Password"}
+                                type="password"
+                                name="password"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                        </div>
+                        
+                        {formErrors.password.length > 0 && (
+                            <span className="errorMessage">{formErrors.password}</span>
+                        )}
+                        
+                        <div className="createAccount">
+                        <button onClick={this.handleSubmit}>{"login"}
+                        
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    
+}
