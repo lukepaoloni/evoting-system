2f13c27afec66149bfbd557a50c0f24b48d8d456
Harry Walker <harry.walker.se@hotmail.com>
2019-03-18 16:44:02 +0000

    Partial config, awaiting front end update

--- server/src/config/config.model.ts
+++ server/src/config/config.model.ts
@@ -1,7 +1,22 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity,
+         Entity,
+         PrimaryGeneratedColumn,
+         Column } from 'typeorm';
 
 @Entity('configurations')
 export class Config extends BaseEntity {
   @PrimaryGeneratedColumn()
   readonly id: number;
+
+  @Column ({
+    type: 'date'
+  })
+  startDate: Date;
+
+  @Column ({
+    type: 'date'
+  })
+  endDate: Date; 
+
+  //VoteType to implement.
 }
