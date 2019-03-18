7892daf6bc500c0ed5fc265fc03a884df3deb497
Shezan123 <b4020327@my.shu.ac.uk>
2019-03-18 13:20:30 +0000

    front end
    react front end

---
+++ web/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import HomePage from './Pages/HomePage'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <HomePage />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
