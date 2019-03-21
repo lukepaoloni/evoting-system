1433cc0cffe931c4a2a7ae46fcc5ae828f5e6296
Luke Paoloni <lukepaoloni.me@gmail.com>
2019-03-21 13:03:43 +0000

    ...

--- server/src/vote/preferential/preferential.model.ts
+++ server/src/vote/preferential/preferential.model.ts
@@ -6,36 +6,6 @@ export class Preferential implements IVoteType {
   Limit: number;
 
   public getResult(data: any) {
-    console.log(data)
-
-    let candidateIds = [];
-
-    // for (let i = 0; i < data.length; i++) {}
-    // data.forEach(el,  => {
-    //   candidateIds[]
-    // });
-
-    // // console.log(data);
-    // let iceCream = [];
-    // let i = 0;
-    // let frequency = {};
-    // data.forEach(element => {
-    //   iceCream[i] = element.candidateId;
-    //   i++;
-    // });
-    // // console.log(iceCream);
-    // iceCream.forEach(function(value) { frequency[value] = 0; });
-
-    // let uniques = iceCream.filter(function(value) {
-    //     return ++frequency[value] == 1;
-    // });
-    // // let test = uniques.sort(function(a, b) {     //  Keep this, it is used to sort them in order might use it later
-    // //     return frequency[b] - frequency[a];
-    // // });
-    // // console.log(test);
-    // console.log(frequency);
-    // return frequency;
+    return data;
   }
-
-
 }
